7f624531c60ee06220aa4d4d26b74cd401fea3d6
DevriesL <therkduan@gmail.com>
2021-04-13 17:10:15 +0800

    Parse native json result
    Signed-off-by: DevriesL <therkduan@gmail.com>

--- app/src/main/java/io/github/devriesl/raptormark/data/NativeDataSource.kt
+++ app/src/main/java/io/github/devriesl/raptormark/data/NativeDataSource.kt
@@ -3,7 +3,9 @@ package io.github.devriesl.raptormark.data
 import android.os.Handler
 import android.os.HandlerThread
 import android.os.Process.THREAD_PRIORITY_FOREGROUND
+import android.util.Log
 import androidx.annotation.Keep
+import org.json.JSONObject
 
 object NativeDataSource {
     private val nativeThread: HandlerThread =
@@ -29,6 +31,48 @@ object NativeDataSource {
     @Keep
     @JvmStatic
     private fun updateStatus(msg: String): Int {
+        parseJsonResult(msg)
         return msg.length
     }
+
+    private fun parseJsonResult(msg: String) {
+        var jobsId: String? = null
+        var jobsRw = String()
+        var sumOfBwBytes: Long = 0
+        var sumOf4NClatNs: Long = 0
+
+        val jsonResult = JSONObject(msg)
+        val jobsArray = jsonResult.getJSONArray("jobs")
+        for (i in 0 until jobsArray.length()) {
+            val jobObject: JSONObject = jobsArray.getJSONObject(i)
+
+            if (jobsId.isNullOrEmpty()) {
+                jobsId = jobObject.getString("jobname")
+                when {
+                    jobsId.contains("rd") -> {
+                        jobsRw = "read"
+                    }
+                    jobsId.contains("wr") -> {
+                        jobsRw = "write"
+                    }
+                    else -> {
+                        Log.e(TAG, "Unknown jobs RW type:$jobsId")
+                    }
+                }
+            }
+
+            val rwObject: JSONObject = jobObject.getJSONObject(jobsRw)
+            sumOfBwBytes += rwObject.getLong("bw_bytes")
+            val clatObject: JSONObject = rwObject.getJSONObject("clat_ns")
+            val percentileObject: JSONObject = clatObject.getJSONObject("percentile")
+            sumOf4NClatNs += percentileObject.getLong("99.990000")
+        }
+
+        val sumOfBw = sumOfBwBytes / 1000 / 1000
+        val avgOf4NClat = sumOf4NClatNs / jobsArray.length() / 1000
+
+        Log.i(TAG, jobsId + ": bw=" + sumOfBw + "MB/s, 99.99% Latency=" + avgOf4NClat + "µs")
+    }
+
+    private const val TAG = "Native_DataSrc"
 }
