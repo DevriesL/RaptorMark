23b9aeddc0c3d17da71542de11ad7be9759e51e2
Devries <therkduan@gmail.com>
2021-05-03 11:23:48 +0800

    Add 99.99% latency text and refactor test item layout
    Signed-off-by: Devries <therkduan@gmail.com>

--- app/src/main/java/io/github/devriesl/raptormark/viewmodels/BenchmarkTestViewModel.kt
+++ app/src/main/java/io/github/devriesl/raptormark/viewmodels/BenchmarkTestViewModel.kt
@@ -1,9 +1,11 @@
 package io.github.devriesl.raptormark.viewmodels
 
+import android.view.View
 import androidx.lifecycle.ViewModel
 import io.github.devriesl.raptormark.data.TestRepository
 
 class BenchmarkTestViewModel(testRepo: TestRepository) : ViewModel() {
 
     val testName = testRepo.getTestName()
+    var randLatVis = if (testRepo.isRandTest) { View.VISIBLE } else { View.GONE }
 }
