16d86a657d72f444e51af2538a28c0860cd00c9e
Sanlorng <fujia625@gmail.com>
2022-02-12 19:23:29 +0800

    Update SingleChoiceDialog design

--- app/src/main/java/io/github/devriesl/raptormark/ui/setting/SingleChoiceDialog.kt
+++ app/src/main/java/io/github/devriesl/raptormark/ui/setting/SingleChoiceDialog.kt
@@ -2,15 +2,14 @@ package io.github.devriesl.raptormark.ui.setting
 
 import androidx.annotation.StringRes
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Divider
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.RadioButton
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -19,6 +18,7 @@ import androidx.compose.ui.window.Dialog
 @Composable
 fun SingleChoiceDialog(
     @StringRes title: Int,
+    defaultChoice: String,
     choiceList: ArrayList<String>,
     itemIndex: Int,
     closeDialog: (Int, String?) -> Unit,
@@ -29,18 +29,40 @@ fun SingleChoiceDialog(
                 .fillMaxWidth()
                 .wrapContentHeight()
         ) {
-            Column(modifier = Modifier.padding(16.dp)) {
-                Text(text = stringResource(title))
-                Divider(Modifier.padding(top = 8.dp))
+            Column {
+                Text(
+                    text = stringResource(title),
+                    style = MaterialTheme.typography.h6,
+                    modifier = Modifier
+                        .padding(horizontal = 16.dp)
+                        .padding(bottom = 8.dp)
+                        .defaultMinSize(minHeight = 40.dp)
+                        .wrapContentHeight(Alignment.Bottom)
+                )
                 LazyColumn(modifier = Modifier.wrapContentHeight()) {
                     items(choiceList) { choice ->
-                        Text(text = choice, Modifier
-                            .clickable { closeDialog(itemIndex, choice) }
-                            .fillMaxWidth()
-                            .padding(vertical = 8.dp))
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                            modifier = Modifier
+                                .fillParentMaxWidth()
+                                .defaultMinSize(minHeight = 48.dp)
+                                .clickable { closeDialog(itemIndex, choice) }
+                        ) {
+                            RadioButton(
+                                selected = defaultChoice == choice,
+                                onClick = null,
+                                modifier = Modifier.padding(
+                                    start = 16.dp,
+                                    end = 16.dp
+                                )
+                            )
+                            Text(text = choice)
+                        }
                     }
                 }
+                Spacer(modifier = Modifier.height(16.dp))
             }
+
         }
     }
 }
